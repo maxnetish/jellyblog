29af6821f09ccf8ae1110337a6be5e204e190b3f
mxmgrd@gmail.com <mxmgrd@gmail.com>
2014-08-17 15:35:48 +0000

    susanin path in public app

--- webapps/public/app.js
+++ webapps/public/app.js
@@ -14,10 +14,56 @@
 
     require(
         [
-            'show-full-content'
+            'show-full-content',
+            'susanin-path',
+            'jquery'
         ],
-        function (showFullContent) {
+        function (showFullContent, path, $) {
             //bind event handlers
             showFullContent.bind();
+
+            // demo path:
+            path.map('/demo/another')
+                .enter(function (parsed) {
+                    console.log('enter url /demo/another');
+                    console.dir(parsed);
+                })
+                .to(function (parsed) {
+                    console.log('to url /demo/another');
+                    console.dir(parsed);
+                })
+                .exit(function (parsed) {
+                    console.log('exit url /demo/another');
+                    console.dir(parsed);
+                });
+            path.map('/demo')
+                .enter(function (parsed) {
+                    console.log('enter url /demo');
+                    console.dir(parsed);
+                })
+                .to(function (parsed) {
+                    console.log('to url /demo');
+                    console.dir(parsed);
+                })
+                .exit(function (parsed) {
+                    console.log('exit url /demo');
+                    console.dir(parsed);
+                });
+            path.rescue(function (path) {
+                // try to load from server
+                console.log('Rescue path: ' + path);
+            });
+            path.listen(); // no fallback to hashbangs
+            $('body').on('click', 'a[data-route]', function (e) {
+                var $this = $(this),
+                    urlAttr = $this.data('route') || 'href',
+                    href = $this.attr(urlAttr);
+                if (href) {
+                    e.preventDefault();
+                    path.pushState(null, null, href);
+                }
+            });
+
+
         });
 })();
