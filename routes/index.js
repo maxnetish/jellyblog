ddc4721ebdd243d492cfe54f581a974963160928
Maksim Gordeev <mxmgrd@gmail.com>
2015-06-01 20:34:20 +0300

    All broken

--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,53 @@
 var express = require('express'),
-    router = express.Router(),
-    indexVm = require('../service/vm/indexVm'),
-    urlHelper = require('../service/urlHelper'),
-    gAnalytics = require('../config').googleAnalytics || {};
+    router = express.Router();
+    //indexVm = require('../service/vm/indexVm'),
+    //urlHelper = require('../service/urlHelper'),
+    //gAnalytics = require('../config').googleAnalytics || {};
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var preferredLocale = req.preferredLocale,
-        skip = parseInt(req.query.skip, 10) || 0,
-        tag = req.query.tag;
 
-    indexVm.promise({
-        preferredLocale: preferredLocale,
-        skip: skip,
-        user: req.user,
-        admin: req.userHasAdminRights,
-        url: req.originalUrl,
-        tag: tag
-    })
-        .then(function (vm) {
-            vm.pageUrl = urlHelper.combine(urlHelper.hostUrl, req.originalUrl);
-            vm.deviceMobile = !!req.detectUserAgent.mobile();
-            vm.gaIdentificator = gAnalytics.identificator;
-            res.render('public/index', vm);
-            return vm;
-        })
-        .then(null, next);
+    res.render('index', {title: 'Cool title'});
+
+    //var preferredLocale = req.preferredLocale,
+    //    skip = parseInt(req.query.skip, 10) || 0,
+    //    tag = req.query.tag;
+    //
+    //indexVm.promise({
+    //    preferredLocale: preferredLocale,
+    //    skip: skip,
+    //    user: req.user,
+    //    admin: req.userHasAdminRights,
+    //    url: req.originalUrl,
+    //    tag: tag
+    //})
+    //    .then(function (vm) {
+    //        vm.pageUrl = urlHelper.combine(urlHelper.hostUrl, req.originalUrl);
+    //        vm.deviceMobile = !!req.detectUserAgent.mobile();
+    //        vm.gaIdentificator = gAnalytics.identificator;
+    //        res.render('public/index', vm);
+    //        return vm;
+    //    })
+    //    .then(null, next);
 });
 
-router.get('/demo', function (req, res, next) {
-    var preferredLocale = req.preferredLocale,
-        skip = parseInt(req.query.skip, 10) || 0;
-
-    indexVm.promise({
-        preferredLocale: preferredLocale,
-        skip: skip,
-        user: req.user,
-        admin: req.userHasAdminRights,
-        url: req.url
-    })
-        .then(function (vm) {
-            res.render('public/demo', vm);
-            return vm;
-        })
-        .then(null, next);
-});
+//router.get('/demo', function (req, res, next) {
+//    var preferredLocale = req.preferredLocale,
+//        skip = parseInt(req.query.skip, 10) || 0;
+//
+//    indexVm.promise({
+//        preferredLocale: preferredLocale,
+//        skip: skip,
+//        user: req.user,
+//        admin: req.userHasAdminRights,
+//        url: req.url
+//    })
+//        .then(function (vm) {
+//            res.render('public/demo', vm);
+//            return vm;
+//        })
+//        .then(null, next);
+//});
 
 module.exports = router;
