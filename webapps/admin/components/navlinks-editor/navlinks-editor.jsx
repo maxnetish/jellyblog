e1faa88e1f6e76759a326f2ad66a8baa88087691
Maksim Gordeev <mxmgrd@gmail.com>
2015-06-15 01:55:57 +0300

    Navlinks editor

--- webapps/admin/components/navlinks-editor/navlinks-editor.jsx
+++ webapps/admin/components/navlinks-editor/navlinks-editor.jsx
@@ -4,6 +4,71 @@ var _ = require('lodash');
 var ClassSet = require('classnames');
 var componentFlux = require('./navlinks-editor-flux');
 
+function renderNavlink(navlinkModel) {
+    return <tbody>
+    <tr>
+        <td>
+            <div>
+                <input type="text"
+                       value={navlinkModel.text}
+                       required
+                       className="form-control"/>
+            </div>
+        </td>
+        <td>
+            <div>{navlinkModel.icon}</div>
+        </td>
+        <td>
+            <div><input type="checkbox" value={navlinkModel.disabled}/></div>
+        </td>
+        <td>
+            <div><input type="checkbox" value={navlinkModel.visible}/></div>
+        </td>
+        <td>
+            <div><input type="checkbox" value={navlinkModel.newWindow}/></div>
+        </td>
+        <td>
+            <div><input type="checkbox" value={navlinkModel.useClientRouter}/></div>
+        </td>
+    </tr>
+    <tr>
+        <td colSpan="6">
+            <div>
+                <input type="url"
+                       value={navlinkModel.url}
+                       required
+                       className="form-control"/>
+            </div>
+        </td>
+    </tr>
+    </tbody>;
+}
+
+function renderTableByCategory(navlinks, category) {
+    var navlinksOfCategory = _.where(navlinks, {
+        category: category
+    });
+
+    return <div ckassName="table-responsive">
+        <table className="table">
+            <caption>{category}</caption>
+            <thead>
+            <tr>
+                <th>Text/URL</th>
+                <th>Icon</th>
+                <th>Disabled</th>
+                <th>Visible</th>
+                <th>New window</th>
+                <th>Use client router</th>
+            </tr>
+            </thead>
+            {_.map(navlinksOfCategory, function (navlink) {
+                return renderNavlink(navlink);
+            })}
+        </table>
+    </div>;
+}
+
 var NavlinksEditor = React.createClass({
     mixins: [Reflux.ListenerMixin],
     getInitialState: function () {
@@ -15,16 +80,10 @@ var NavlinksEditor = React.createClass({
             navlinks-editor
             <div className="panel panel-default">
                 <div className="panel-heading">
-                    <span className="panel-title">Main menu links</span>
+                    <span className="panel-title">Links</span>
                 </div>
                 <div className="panel-body">
-                </div>
-            </div>
-            <div className="panel panel-default">
-                <div className="panel-heading">
-                    <span className="panel-title">Footer links</span>
-                </div>
-                <div className="panel-body">
+                    {renderTableByCategory(this.state.data, 'main')}
                 </div>
             </div>
         </section>;
