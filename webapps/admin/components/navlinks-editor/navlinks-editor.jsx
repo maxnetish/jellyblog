09485ed8b979c5d10e1853d02d45a31db1cc7347
Maksim Gordeev <mxmgrd@gmail.com>
2015-06-15 19:41:45 +0300

    Lovelight

--- webapps/admin/components/navlinks-editor/navlinks-editor.jsx
+++ webapps/admin/components/navlinks-editor/navlinks-editor.jsx
@@ -5,67 +5,51 @@ var ClassSet = require('classnames');
 var componentFlux = require('./navlinks-editor-flux');
 
 function renderNavlink(navlinkModel) {
-    return <tbody>
-    <tr>
-        <td>
-            <div>
-                <input type="text"
-                       value={navlinkModel.text}
-                       required
-                       className="form-control"/>
+    var iconClass = 'navlink-icon glyphicon ' + navlinkModel.icon;
+    var linkClass = ClassSet({
+        'navlink-ancor': true,
+        'disabled': navlinkModel.disabled,
+        'menu-preview-no-visible': !navlinkModel.visible
+    });
+    var linkTarget = navlinkModel.newWindow ? '_blank' : null;
+
+    return <li className="list-group-item">
+        <div className="row">
+            <div className="col-md-8">
+                <a className={linkClass}
+                   target={linkTarget}
+                   href={navlinkModel.url}>
+                    {navlinkModel.icon ? <i className={iconClass}></i> : null}
+                    <span className="navlink-text">{navlinkModel.text}</span>
+                </a>
             </div>
-        </td>
-        <td>
-            <div>{navlinkModel.icon}</div>
-        </td>
-        <td>
-            <div><input type="checkbox" value={navlinkModel.disabled}/></div>
-        </td>
-        <td>
-            <div><input type="checkbox" value={navlinkModel.visible}/></div>
-        </td>
-        <td>
-            <div><input type="checkbox" value={navlinkModel.newWindow}/></div>
-        </td>
-        <td>
-            <div><input type="checkbox" value={navlinkModel.useClientRouter}/></div>
-        </td>
-    </tr>
-    <tr>
-        <td colSpan="6">
-            <div>
-                <input type="url"
-                       value={navlinkModel.url}
-                       required
-                       className="form-control"/>
+            <div className="col-md-4 text-right">
+                <div className="btn-group btn-group-xs" role="group">
+                    <button type="button" className="btn btn-xs btn-primary" title="Edit">
+                        <i className="glyphicon glyphicon-edit"></i>
+                        &nbsp;Edit
+                    </button>
+                    <button type="button" className="btn btn-xs btn-danger" title="Remove">
+                        <i className="glyphicon glyphicon-remove"></i>
+                    </button>
+                </div>
             </div>
-        </td>
-    </tr>
-    </tbody>;
+        </div>
+    </li>;
 }
 
-function renderTableByCategory(navlinks, category) {
+function renderLinksByCategory(navlinks, category) {
     var navlinksOfCategory = _.where(navlinks, {
         category: category
     });
 
-    return <div ckassName="table-responsive">
-        <table className="table">
-            <caption>{category}</caption>
-            <thead>
-            <tr>
-                <th>Text/URL</th>
-                <th>Icon</th>
-                <th>Disabled</th>
-                <th>Visible</th>
-                <th>New window</th>
-                <th>Use client router</th>
-            </tr>
-            </thead>
+    return <div>
+        <h4>{category}</h4>
+        <ul className="list-group">
             {_.map(navlinksOfCategory, function (navlink) {
                 return renderNavlink(navlink);
-            })}
-        </table>
+            }, this)}
+        </ul>
     </div>;
 }
 
@@ -76,14 +60,21 @@ var NavlinksEditor = React.createClass({
         return vm;
     },
     render: function () {
-        return <section>
+        return <section className="navlink-editor">
             navlinks-editor
             <div className="panel panel-default">
                 <div className="panel-heading">
                     <span className="panel-title">Links</span>
                 </div>
                 <div className="panel-body">
-                    {renderTableByCategory(this.state.data, 'main')}
+                    <div className="row">
+                        <div className="col-md-6">
+                            {renderLinksByCategory(this.state.data, 'main')}
+                        </div>
+                        <div className="col-md-6">
+                            {renderLinksByCategory(this.state.data, 'footer')}
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>;
