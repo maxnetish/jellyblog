08a94f2624f4945153b3113d6335b032fcc1ac95
Maksim Gordeev <mxmgrd@gmail.com>
2016-12-04 23:41:26 +0400

    Setup grunt & webpack

---
+++ Gruntfile.js
@@ -0,0 +1,161 @@
+module.exports = function (grunt) {
+
+    var buildDir = 'build';
+    var srcDir = 'src';
+    var mainAppFile = 'server.js';
+    var webpack = require('webpack');
+    var webpackCommonOptions = require('./webpack.config.js');
+    var path = require('path');
+
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        clean: [
+            buildDir
+        ],
+
+        copy: {
+
+        },
+
+        babel: {
+            'dev': {
+                options: {
+                    sourceMap: true,
+                    presets: ['react', 'es2015'],
+                    // plugins: ['transform-es2015-modules-commonjs'],
+                    // auxiliaryCommentBefore: 'Babel jsx transform:',
+                    // auxiliaryCommentAfter: 'end of jsx transform',
+                    ast: false
+                },
+                files: [
+                    {
+                        expand: true,
+                        filter: 'isFile',
+                        cwd: 'src/',
+                        src: ['**/*.js*'],
+                        dest: buildDir + '/'
+                    }
+                ]
+            },
+            'prod': {
+                options: {
+                    sourceMap: false,
+                    presets: ['react', 'es2015'],
+                    // uglify2JS doesn't support es6
+                    // plugins: [
+                    //     'transform-es2015-modules-commonjs',
+                    //     'transform-es2015-template-literals'
+                    // ],
+                    ast: false
+                },
+                files: [
+                    {
+                        expand: true,
+                        filter: 'isFile',
+                        cwd: 'src/',
+                        src: ['**/*.js*'],
+                        dest: buildDir + '/'
+                    }
+                ]
+            }
+        },
+
+        webpack: {
+            options: webpackCommonOptions,
+            dev: {
+                devtool: "cheap-module-inline-source-map",
+                debug: true
+            },
+            prod: {
+                plugins: webpackCommonOptions.plugins.concat([
+                    new webpack.DefinePlugin({
+                        "process.env": {
+                            // This has effect on the react lib size
+                            "NODE_ENV": JSON.stringify("production")
+                        }
+                    }),
+                    new webpack.optimize.DedupePlugin(),
+                    new webpack.optimize.UglifyJsPlugin()
+                ])
+            }
+        },
+
+        less: {
+            dev: {
+                files: [{
+                    src: 'src/react-app/**/*.less',
+                    dest: buildDir + '/bundle.css'
+                }],
+                options: {
+                    sourceMap: true,
+                    outputSourceFiles: true,
+                    plugins: [
+                        new (require('less-plugin-npm-import')),
+                        new (require('less-plugin-autoprefix'))({
+                            browsers: ['last 2 versions']
+                        })
+                    ]
+                }
+            },
+            prod: {
+                files: [{
+                    src: 'src/react-app/**/*.less',
+                    dest: buildDir + '/bundle.css'
+                }],
+                options: {
+                    sourceMap: false,
+                    plugins: [
+                        new (require('less-plugin-npm-import')),
+                        new (require('less-plugin-autoprefix'))({
+                            browsers: ['last 2 versions']
+                        })
+                    ]
+                }
+            }
+        },
+
+        delta: {
+            options: {
+                livereload: false
+            },
+
+            /**
+             * When our JavaScript source files change, we want to browserify
+             * but uglifying really not needed
+             */
+            'js-files': {
+                files: [srcDir + '/**/*.js*'],
+                tasks: ['babel:dev', 'webpack:dev']
+            },
+            // 'html-files': {
+            //     files: [srcDir + '/**/*.html'],
+            //     tasks: ['copy:index2Build']
+            // },
+
+            /**
+             * When the LESS files change, we need to compile them.
+             * but not minify
+             */
+            less: {
+                files: ['src/react-app/**/*.less'],
+                tasks: ['less:dev']
+            }
+        }
+    });
+
+    /**
+     * In order to make it safe to just compile or copy *only* what was changed,
+     * we need to ensure we are starting from a clean, fresh build. So we rename
+     * the `watch` task to `delta` (that's why the configuration var above is
+     * `delta`) and then add a new task called `watch` that does a clean build
+     * before watching for changes.
+     */
+    grunt.renameTask('watch', 'delta');
+
+    grunt.registerTask('dev', ['clean', 'babel:dev', 'webpack:dev', 'less:dev', 'delta']);
+    grunt.registerTask('prod', ['clean', 'babel:prod', 'webpack:prod', 'less:prod']);
+};
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    cache: true,
+
+    // entry: './build/client.js',
+    entry: {
+        'client': './build/client.js',
+        'common': ['react', 'react-dom', 'core-js/es6/promise', 'superagent', 'reflux']
+        // vendor: ['core-js/es6/promise', 'whatwg-fetch', 'react', 'react-dom']
+        // jquery: "./app/jquery",
+        // bootstrap: ["!bootstrap-webpack!./app/bootstrap/bootstrap.config.js", "./app/bootstrap"],
+        // react: "./app/react"
+    },
+    output: {
+        path: 'build/pub',
+        filename: '[name].js',
+        chunkFilename: '[name].[id].chunk.js',
+        publicPath: '/'
+    },
+    // output: {
+    //     path: 'build/assets',
+    //     filename: "[name].bundle.js",
+    // },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('common', 'common.js')
+    ],
+    module: {
+        preLoaders: [
+            {loaders: ['isomorphine']}
+        ],
+
+        loaders: [
+            // make sure to exclude route components here
+            // {
+            //     test: /\.js$/,
+            //     include: path.resolve(__dirname, 'build'),
+            //     exclude: routeComponentRegex,
+            //     loader: 'console'
+            // },
+            // lazy load route components
+            // {
+            //     test: routeComponentRegex,
+            //     include: path.resolve(__dirname, 'build'),
+            //     loaders: ['console', 'bundle?lazy']
+            // }
+            // // required to write "require('./style.css')"
+            // { test: /\.css$/,    loader: "style-loader!css-loader" },
+            //
+            // // required for bootstrap icons
+            // { test: /\.woff$/,   loader: "url-loader?prefix=font/&limit=5000&mimetype=application/font-woff" },
+            // { test: /\.ttf$/,    loader: "file-loader?prefix=font/" },
+            // { test: /\.eot$/,    loader: "file-loader?prefix=font/" },
+            // { test: /\.svg$/,    loader: "file-loader?prefix=font/" },
+
+            // required for react jsx
+            // { test: /\.js$/,    loader: "jsx-loader" },
+            // { test: /\.jsx$/,   loader: "jsx-loader?insertPragma=React.DOM" },
+        ]
+    },
+    resolve: {
+        alias: {
+            // prect-compat:
+            // 'preact-compat/lib/ReactCSSTransitionGroup$': 'react/lib/ReactCSSTransitionGroup',
+            // "react": "preact-compat",
+            // "react-dom": "preact-compat",
+            // 'react-addons-css-transition-group': 'rc-css-transition-group'
+
+            // Bind version of jquery
+            // jquery: "jquery-2.0.3",
+
+            // Bind version of jquery-ui
+            // "jquery-ui": "jquery-ui-1.10.3",
+
+            // jquery-ui doesn't contain a index file
+            // bind module to the complete module
+            // "jquery-ui-1.10.3$": "jquery-ui-1.10.3/ui/jquery-ui.js",
+        }
+    }/*,
+     plugins: [
+     new webpack.ProvidePlugin({
+     // Automtically detect jQuery and $ as free var in modules
+     // and inject the jquery library
+     // This is required by many jquery plugins
+     jQuery: "jquery",
+     $: "jquery"
+     })
+     ]*/
+};
