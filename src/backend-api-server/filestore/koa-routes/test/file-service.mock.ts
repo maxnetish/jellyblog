d5f5d5a0c21480b5922be1268edc2c79ac408ba0
Maksim Gordeev <mxmgrd@gmail.com>
2019-12-09 18:42:30 +0300

    Corrected test

--- src/backend-api-server/filestore/koa-routes/test/file-service.mock.ts
+++ src/backend-api-server/filestore/koa-routes/test/file-service.mock.ts
@@ -54,6 +54,7 @@ export class FileServiceMock implements IFileService {
             fieldname: '',
             destination: '',
             buffer: Buffer.from([]),
+            location: '',
         };
     }
 
